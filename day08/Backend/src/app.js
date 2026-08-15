004f696ddbbb9eb14252bc03165004bd4da0fca4
tiwari786 <vipultiwari479@gmail.com>
2026-08-15 21:54:26 +0530

    frontend integration complete

--- day08/Backend/src/app.js
+++ day08/Backend/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const noteModel = require("./models/note.model");
 const cors = require("cors")
+const path = require("path")
 
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static("./public"))
 
 // POST
 app.post("/api/notes", async (req, res) => {
@@ -52,6 +55,10 @@ app.patch("/api/notes/:id", async (req, res) => {
     res.status(200).json({
         message: "Note updated successfully"
     })
+})
+
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, +".." + "/public/index.html"))
 })
 
 module.exports = app
