f471bace5fedc10a3abca254299f413e42d3705e
tiwari786 <vipultiwari479@gmail.com>
2026-08-15 21:56:45 +0530

    integration complete

--- day08/Frontend/src/App.jsx
+++ day08/Frontend/src/App.jsx
@@ -1,32 +1,83 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios"
 
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      title: "title 1",
-      description: "desc 1"
-    },
-    {
-      title: "title 2",
-      description: "desc 2"
-    }
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get("http://localhost:3000/api/notes")
-    .then((res) => {
-      setNotes(res.data.notes)
+  const fecthNotes = () => {
+    axios.get("http://localhost:3000/api/notes")
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
+  }
+
+
+  useEffect(() => {
+    fecthNotes()
+  }, [])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { title, description } = e.target.elements
+
+    console.log(title.value, description.value)
+
+    axios.post("http://localhost:3000/api/notes", {
+      title: title.value,
+      description: description.value
+    }).then(res => {
+      console.log(res.data)
+      fecthNotes()
     })
+
+  }
+
+
+
+  const handleDelete = (noteId) => {
+    axios.delete(`http://localhost:3000/api/notes/${noteId}`)
+      .then(res => {
+        console.log("Note deleted")
+        fecthNotes()
+      })
+  }
+
+  // const handleEdit = (noteId, e) => {
+  //   // e.preventDefault()
+
+  //   const { title, description } = e.target.elements
+
+  //   axios.patch(`http://localhost:3000/api/notes/${noteId}`, {
+  //     title: title.value,
+  //     description: description.value
+  //   })
+  //     .then(res => {
+  //       console.log(res.data)
+  //       fecthNotes()
+  //     })
+  // }
+
 
   return (
     <>
+
+      <form className='' onSubmit={handleSubmit}>
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button>Create note</button>
+      </form>
+
       <div className="notes">
         {
           notes.map(note => {
-            return <div className="note">
+            return <div className="note" key={note._id}>
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+              <button onClick={() => { handleDelete(note._id) }}>Delete</button>
+              {/* <button onClick={() => { handleEdit(note._id) }}>Edit</button> */}
             </div>
           })
         }
